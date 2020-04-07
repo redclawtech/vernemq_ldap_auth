bf3734fa2dd01267e466ef580c53760b05f3f30b
Dairon Medina Caro <dairon@gooee.com>
2020-04-06 21:46:16 -0400

    Include eldap library

--- src/vernemq_ldap_auth.erl
+++ src/vernemq_ldap_auth.erl
@@ -1,6 +1,11 @@
 -module(vernemq_ldap_auth).
 
+-include_lib("eldap/include/eldap.hrl").
 -include_lib("vernemq_dev/include/vernemq_dev.hrl").
+
+%% Number of times to retry LDAP operations.
+-define(LDAP_RETRIES, 10).
+
 
 
 
