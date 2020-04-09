8ad7dbb54c9001a277ab333f12a9e316d81b6904
Dairon Medina Caro <dairon@gooee.com>
2020-04-09 00:43:46 -0400

    Add plugin behaviours and hooks

--- src/vernemq_ldap_auth.erl
+++ src/vernemq_ldap_auth.erl
@@ -3,8 +3,43 @@
 -include_lib("eldap/include/eldap.hrl").
 -include_lib("vernemq_dev/include/vernemq_dev.hrl").
 
-%% Number of times to retry LDAP operations.
--define(LDAP_RETRIES, 10).
+-behaviour(auth_on_register_hook).
+-behaviour(auth_on_register_m5_hook).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% Plugin Callbacks
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+start() ->
+    {ok, _} = application:ensure_all_started(vernemq_ldap_auth),
+    vernemq_ldap_auth_cli:register(),
+    ok.
+
+stop() ->
+    application:stop(vernemq_ldap_auth).
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% VerneMQ Hooks
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+auth_on_register(_Peer, _SubscriberId, User, Password, _CleanSession) ->
+    authenticate(User, Password).
+
+auth_on_register_m5(_Peer, _SubscriberId, User, Password, _CleanStart, _Properties) ->
+    authenticate(User, Password).
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% Internal
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+authenticate(undefined, _) ->
+    next;
+authenticate(_, undefined) ->
+    next;
+authenticate(User, Password) ->
+    ok.
 
 
 
