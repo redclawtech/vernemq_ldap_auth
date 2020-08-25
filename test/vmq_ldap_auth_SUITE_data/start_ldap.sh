aaa2fe5454cdb47d6ade0f00461fd63c03567583
Dairon Medina Caro <dairon@gooee.com>
2020-08-24 21:18:35 -0400

    Adding ldap config

--- test/vmq_ldap_auth_SUITE_data/start_ldap.sh
+++ test/vmq_ldap_auth_SUITE_data/start_ldap.sh
@@ -4,15 +4,17 @@ set -x
 
 $bind_port=$1
 
+uri="ldap://localhost:$tcp_port"
+
 SLAPADD=/usr/sbin/slapadd
 SLAPD=/usr/sbin/slapd
 
 setup_slapd() {
     set -e
     mkdir -p $WORKDIR/ldap
-
+    $SLAPD -h "$uri" -f $ARTIFACTS/slapd.conf &
     attempts=0
-    until ldapsearch -x -H ldapi://${WORKDIR//\//%2F}%2Fldapi -b "dc=example,dc=com" '(objectclass=*)'; do
+    until ldapsearch -x -H "$uri" -b "dc=example,dc=com" '(objectclass=*)'; do
         attempts=$(($attempts + 1))
         if [[ $attempts -gt 10 ]]; then
         echo "failed to connect to slapd :("
